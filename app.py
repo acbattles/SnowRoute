700158376b4f158c5044e6893bfead404ee4130c
acbattles <andrewcbattles@gmail.com>
2020-01-29 22:51:13 -0700

    Query database for intersections at turns of route

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
 from flask import Flask, render_template, request
 from mapbox import Geocoder, Directions
 from shapely.geometry import Point, LineString
-import pandas
+import pandas as pd
+import numpy as np
 import geopandas
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+import psycopg2
+from geoalchemy2 import Geometry
 
 
 
 app = Flask(__name__)
 
+#def get_point():
 
+
+def geodb_query(list_x):
+    
+    con = psycopg2.connect(database = 'intersections', user = 'postgres', password = 'andrew17')
+    inters_sql_df = pd.DataFrame()
+    for i in list_x:
+        sql_query = """
+        SELECT * FROM intersections_data_table
+        WHERE ST_Within(geometry, ST_Buffer(ST_MakePoint{x},0.0001));
+        """.format(x=i)
+    
+        inters_sql_df = inters_sql_df.append(pd.read_sql_query(sql_query,con))
+
+    inter_IDs = list(inters_sql_df.MASTERID)
+    return inter_IDs
 
 @app.route('/')
 def input_data():
@@ -35,8 +56,14 @@ def output():
 
     route1 = directions.directions([origin,destination], 'mapbox/driving').geojson()
 
+    #makes a list of the start/end coordinates on each line segment of the route
+    coord_points = route1['features'][0]['geometry']['coordinates']
     
-    return render_template("output.html", the_route = route1)
+    #get the coordinates for TURNS (at this point)
+    intersections_list = geodb_query(coord_points)
+
+    
+    return render_template("output.html", the_route = route1, intersections_list = intersections_list)
 
 
 if __name__ == "__main__":
